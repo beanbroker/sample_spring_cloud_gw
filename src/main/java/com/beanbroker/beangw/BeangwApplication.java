8a1ad2f8abfbb150d76f64a563a741fb74c4982b
beanbroker <88p7h21@gmail.com>
2019-07-21 17:36:37 +0900

    asd

---
+++ src/main/java/com/beanbroker/beangw/BeangwApplication.java
@@ -0,0 +1,13 @@
+package com.beanbroker.beangw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeangwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeangwApplication.class, args);
+    }
+
+}
